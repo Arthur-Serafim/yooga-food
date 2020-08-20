3974e933e5b9fe4244b77c0b92bcdf7dc5709413
Arthur-Serafim <dev.seraph.arthur@gmail.com>
2020-08-20 11:14:44 -0300

    Finish categorypage

--- src/components/CategoryNavbar/CategoryNavbar.tsx
+++ src/components/CategoryNavbar/CategoryNavbar.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./CategoryNavbar.scss";
 
-export default function CategoryNavbar({ categoryName }: any, props: any) {
+export default function CategoryNavbar({ categoryName, props }: any) {
     console.log(categoryName);
     return (
         <nav className="category-navbar">
-            <button className="category-navbar-return-button">
+            <button
+                className="category-navbar-return-button"
+                onClick={() => props.history.push("/busca")}
+            >
                 <i className="fas fa-arrow-left category-navbar-return-icon"></i>
             </button>
             <span>{categoryName}</span>
--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,12 +1,66 @@
 import React from "react";
 import CategoryNavbar from "../../components/CategoryNavbar/CategoryNavbar";
+import { Store } from "../../interfaces";
+import { storeMockup } from "../../mockup";
+import "./Category.scss";
 
 export default function Category(props: any) {
     const category_name = props.match.params.category_name;
+    let stores = storeMockup.filter(
+        (store: Store) => store.description === category_name
+    );
+
+    function handleSelectStore(opened: any) {
+        if (opened) {
+            alert("yupi");
+        }
+    }
+
     return (
-        <div>
-            <CategoryNavbar categoryName={category_name} />
-            <h2>{category_name}</h2>
-        </div>
+        <>
+            <CategoryNavbar categoryName={category_name} props={props} />
+            <div className="category-page-container">
+                <div className="stores-container category-page-container-new">
+                    {stores.map((store: Store) => (
+                        <span
+                            className={`store-display-container ${
+                                !store.opened &&
+                                "store-display-container-closed"
+                            }`}
+                            onClick={() => handleSelectStore(store.opened)}
+                            key={Math.random()}
+                        >
+                            <img
+                                src={store.img}
+                                alt=""
+                                className="store-display-img"
+                            />
+                            <div className="store-display-info-container">
+                                <span className="store-display-title">
+                                    {store.name}
+                                </span>
+                                <div className="store-display-info-description-distance">
+                                    {store.description} - {store.distance}km
+                                </div>
+                                <div
+                                    className="store-display-info-description-distance
+              store-display-info-align-delivery"
+                                >
+                                    {store.delivery_fee ? (
+                                        <span>
+                                            R${" "}
+                                            {store.delivery_fee &&
+                                                store.delivery_fee.toFixed(2)}
+                                        </span>
+                                    ) : (
+                                        <span>Entrega Grátis</span>
+                                    )}
+                                </div>
+                            </div>
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
